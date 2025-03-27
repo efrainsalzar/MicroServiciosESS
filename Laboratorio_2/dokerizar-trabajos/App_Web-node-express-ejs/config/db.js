6e0ba0aed1220a67b16f2b84dc5894a534ed214f
Muss <efrainsalzarsantos@gmail.com>
2025-03-27 10:41:15 -0400

    docker ej 2 del lab 2

--- Laboratorio_2/dokerizar-trabajos/App_Web-node-express-ejs/config/db.js
+++ Laboratorio_2/dokerizar-trabajos/App_Web-node-express-ejs/config/db.js
@@ -7,13 +7,20 @@ const db = mysql.createConnection({
     database: 'user_db'
 });
 
+// Manejo de errores mejorado
 db.connect((err) => {
-    if (err) {
-      console.error('Error conectando a la base de datos: ' + err.stack);
+  if (err) {
+      console.error('Error detallado de conexión a la base de datos:', {
+          message: err.message,
+          code: err.code,
+          errno: err.errno,
+          sqlState: err.sqlState,
+          fatal: err.fatal
+      });
       return;
-    }
-    console.log('Conexión exitosa a la base de datos');
-  });
+  }
+  console.log('Conexión exitosa a la base de datos');
+});
 
 //db.end();
 
